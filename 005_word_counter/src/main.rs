69cbf837e9aca9cf3bcb2c852bbbebd8f264526c
blep <baptiste.lepilleur@gmail.com>
2017-05-30 19:29:56 +0200

    dummy word counting

--- 005_word_counter/src/main.rs
+++ 005_word_counter/src/main.rs
@@ -2,6 +2,23 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+fn count_word(line: String) -> isize {
+	let mut nb_word_found = 0;
+	let mut last_char_was_word = false;
+	for c in line.chars() {
+		let char_is_word = c.is_alphanumeric();
+		if char_is_word && !last_char_was_word {
+			nb_word_found += 1;
+//			write!( std::io::stdout(), "{}", '\n' ).unwrap();
+		}
+		last_char_was_word = char_is_word;
+//		if char_is_word {
+//			write!( std::io::stdout(), "{}", c ).unwrap();
+//		}
+	}
+	nb_word_found
+}
+
 fn run_app() -> Result<(), std::io::Error> {
 	let path = "Cargo.toml";
 	let file = File::open(path);
@@ -10,13 +27,15 @@ fn run_app() -> Result<(), std::io::Error> {
 		Err(err) => return Err(err), // Put nicer error message with path that was not found
 	};
 	let reader = BufReader::new(file);
+	let mut total_word_count = 0;
 	for line in reader.lines() {
 		match line {
-			Ok(line) => println!("Line: {}", line),
+			Ok(line) => total_word_count += count_word( line ),
 			Err(err) => return Err(err),
 		};
 		
 	}
+	println!("{}", total_word_count);
     Ok(())
 }
 
