23321d2775b265ce7724c7ab4f2e2e471cb8060f
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2017-05-31 11:48:55 +0200

    Used rayon library to parallelize processing by line.

--- 005_word_counter/src/main.rs
+++ 005_word_counter/src/main.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
 
-fn count_word(line: String) -> isize {
+extern crate rayon;
+use rayon::prelude::*;
+
+fn count_word(line: &str) -> isize {
 	let mut nb_word_found = 0;
 	let mut last_char_was_word = false;
 	for c in line.chars() {
@@ -22,16 +24,18 @@ fn count_word(line: String) -> isize {
 fn run_app() -> Result<(), std::io::Error> {
 	let path = "Cargo.toml";
 	let file = File::open(path);
-	let file = match file {
+	let mut file = match file {
 		Ok(file) => file,
 		Err(err) => return Err(err), // Put nicer error message with path that was not found
 	};
-	let reader = BufReader::new(file);
-	let total_word_count: Result<isize, std::io::Error> = reader.lines().map(  
-		|line| match line {
-			Ok(line) => Ok(count_word( line )),
-			Err(err) => Err(err),
-		}
+	let mut contents = String::new();
+	match file.read_to_string(&mut contents) {
+		Ok(_) => (),
+		Err(err) => return Err(err),
+	};
+
+	let total_word_count: Result<isize, std::io::Error> = contents.par_lines().map( 
+		|line| Ok(count_word( line ))
 		).sum();
 	match total_word_count {
 		Ok(count) => println!("{}", count),
