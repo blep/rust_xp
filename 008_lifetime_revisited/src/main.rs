7c86bf290549c61c98072539ca23809dceea83f9
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2017-06-08 10:04:53 +0200

    Added checking for Box.

--- 008_lifetime_revisited/src/main.rs
+++ 008_lifetime_revisited/src/main.rs
@@ -1,5 +1,24 @@
+use std::rc::Rc;
+use std::borrow::Borrow;
+
 trait Alive {
     fn alive(&self);
+}
+
+// Implement for single-threaded referenced counted pointer by forwarding to the underlying type.
+impl<T: Alive> Alive for Rc<T> {
+    fn alive(&self) {
+        let obj: &T = self.borrow();
+        obj.alive();
+    }
+}
+
+// Implement for Box
+impl<T: Alive> Alive for Box<T> {
+    fn alive(&self) {
+        let obj: &T = self.borrow();
+        obj.alive();
+    }
 }
 
 
@@ -97,6 +116,8 @@ fn print_alive_by_value<T: Alive>(o: T) {
 
 fn main() {
     println!("> enter main()");
+    
+    // Non-copyable Obj (pass using move semantic)
     let mut o1 = Obj::new( "main" );
     o1.alive();
     print_alive_by_ref(&o1);
@@ -112,6 +133,7 @@ fn main() {
     println!("exit sub-scope, o1_clone should have been destroyed");
     println!("");
     
+    // Copyable Obj (pass using copy)
     let mut o2 = CopyObj::new( "main_copyable" );
     o2.alive();
     print_alive_by_ref(&o2);
@@ -124,5 +146,44 @@ fn main() {
     o2.alive();
     o2_clone.alive();
     println!("");
+    
+    // Ref counted Non-copyable obj
+    let mut ro1 = Rc::new( Obj::new("rc_main") );
+    ro1.alive();
+    print_alive_by_ref(&ro1);
+    print_alive_by_mut_ref(&mut ro1);
+// Q: what trait is needed?!?
+//    print_alive_by_mut_ref(&mut ro1.get_mut().unwrap());
+    print_alive_by_value(ro1); // pass by move semantic
+    println!("returned from print_alive_by_value, rc_main should have been destroyed");
+    println!("");
+    
+    // Ref counted Non-copyable obj (keeping another reference on the ref counted pointer)
+    let ro2 = Rc::new( Obj::new("rc_main2") );
+    ro2.alive();
+    {
+        let ro2_clone = ro2.clone(); // "copy" the ref count ptr, increasing the number of reference
+        print_alive_by_value(ro2); // pass by move semantic
+        ro2_clone.alive();
+    }
+    println!("exit sub-scope, rc_main2 should have been destroyed");
+    println!("");
+    
+    // Boxed non-copyable obj (hold a pointer to a dynamically allocated Obj)
+    let mut bo1 = Box::new( Obj::new("box_main") );
+    bo1.alive();
+    print_alive_by_ref(&bo1);
+    print_alive_by_mut_ref(&mut bo1);
+    print_alive_by_ref(&*bo1);
+    {
+        let mut bo1_clone = bo1.clone(); // allocate a new clone of the pointed value
+        (*bo1_clone).name = "box_main_clone".to_string();
+        bo1_clone.alive();
+        print_alive_by_value(bo1); // pass by move semantic
+        println!("returned from print_alive_by_value, box_main should have been destroyed");
+    }
+    println!("exit sub-scope, box_main_clone should have been destroyed");
+    println!("");
+    
     println!("< exit main()");
 }
